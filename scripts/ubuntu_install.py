d60b4c07e889a001a18ee0ca81f89690de8e4527
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-08 15:16:24 +0300

    upgrade for php 7.0

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -8,12 +8,12 @@ ubuntu to compile and work with this package.
 import subprocess # for check_call
 
 packs=[
-	'libapache2-mod-php5',
-	'php5',
-	'php5-json',
-	'php5-mysql',
-	'php5-readline',
-	'php5-cli',
+	'libapache2-mod-php7.0',
+	'php7.0',
+	'php7.0-json',
+	'php7.0-mysql',
+	'php7.0-readline',
+	'php7.0-cli',
 ]
 
 args=['sudo','apt-get','install','--assume-yes']
