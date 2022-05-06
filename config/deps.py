d395036ce1689d20f87445b409e872810a0aacd0
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 03:43:31 +0300

    removed packages

--- config/deps.py
+++ config/deps.py
@@ -1,8 +1,9 @@
 packages=[
-    "libapache2-mod-php7.0",
-    "php7.0",
-    "php7.0-json",
-    "php7.0-mysql",
-    "php7.0-readline",
-    "php7.0-cli",
+    # could find in the CI environment
+    # "libapache2-mod-php7.0",
+    # "php7.0",
+    # "php7.0-json",
+    # "php7.0-mysql",
+    # "php7.0-readline",
+    # "php7.0-cli",
 ]
