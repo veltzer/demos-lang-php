cd6338741345290e5a9074fb60e00a08d1c159b9
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:57 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-php"
-project_long_description = "Demos for the php language"
-project_year_started = 2015
-project_description = """Demos for the php language"""
+description_short = "Demos for the php language"
