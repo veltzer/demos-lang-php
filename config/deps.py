06b8e7e38733378622128b19c6b6dfbfdc887c16
Mark Veltzer <mark.veltzer@gmail.com>
2024-08-30 20:30:47 +0300

    no commit message given

--- config/deps.py
+++ config/deps.py
@@ -1,9 +1,7 @@
 packages=[
-    # could find in the CI environment
-    # "libapache2-mod-php7.0",
-    # "php7.0",
-    # "php7.0-json",
-    # "php7.0-mysql",
-    # "php7.0-readline",
-    # "php7.0-cli",
+    "php",
+    "php-cli",
+    "php-mysql",
+    "php-readline",
+    "php-json",
 ]
